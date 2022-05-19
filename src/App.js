46b4f146140668bf12b375e2e7ae6a9926daab98
frs04 <frs04@mail.aub.edu>
2022-05-20 00:57:52 +0300

    bugfixes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import './App.css';
 import axios from 'axios';
 import { BsSpotify, BsStarFill, BsStar } from 'react-icons/bs';
 import { BiSearchAlt2 } from 'react-icons/bi';
+import { useParams } from "react-router-dom";
 
 function App() {
-  const CLIENT_ID = "5ee3c9c43db84bd291846a15a11913c5"
   const REDIRECT_URI = "http://localhost:3000/"
   const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-  const RESPONSE_TYPE = "token"
 
   const [token, setToken] = useState("")
   const [searchKey, setSearchKey] = useState("")
@@ -16,18 +15,30 @@ function App() {
   const [albums, setAlbums] = useState([])
   const [showArtists, setShowArtists] = useState(true)
   const [albumShow, setAlbumShow] = useState(true);
+  const [loginBox, setLoginBox] = useState(false);
+  const [CLIENT_ID, setCLIENT_ID] = useState('');
+  const [clientSecret, setClientSecret] = useState('');
+  const p = useParams();
 
-  useEffect(() => {
-    const hash = window.location.hash
-    let token = window.localStorage.getItem("token")
+  var access_token = null;
+  var refresh_token = null;
 
-    if (!token && hash) {
-      token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
-      window.location.hash = ""
-      window.localStorage.setItem("token", token)
-    }
-    setToken(token)
-  }, [])
+  function requestAuthorization() {
+    localStorage.setItem("client_id", CLIENT_ID);
+    localStorage.setItem("client_secret", clientSecret); // In a real app you should not expose your client_secret to the user
+
+    let url = AUTH_ENDPOINT;
+    url += "?client_id=" + CLIENT_ID;
+    url += "&response_type=code";
+    url += "&redirect_uri=" + encodeURI(REDIRECT_URI);
+    url += "&show_dialog=true";
+    url += "&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private";
+    window.location.href = url; // Show Spotify's authorization screen
+  }
+
+
+
+
 
   const searchArtists = async (e) => {
     setShowArtists(false)
@@ -60,7 +71,7 @@ function App() {
   const renderArtists = () => {
     return artists.map(artist => (
       <div className="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3">
-        <div className="card" key={artist.id} onClick={(e) => {Albums(e, artist.id); }}>
+        <div className="card" key={artist.id} onClick={(e) => { Albums(e, artist.id); }}>
           {artist.images.length ? <img width={"100%"} class="card-img-top" src={artist.images[0].url} alt="Artist Image" /> : <div>No Image</div>}
           <div className="card-body">
             <h5 className="card-title">{artist.name}</h5>
@@ -148,18 +159,103 @@ function App() {
     ))
 
   }
+
+  function handleAuthorizationResponse() {
+      console.log(this.status)
+
+    if (this.status == 200) {
+      var data = JSON.parse(this.responseText);
+      setToken(data.access_token)
+      console.log(data);
+      var data = JSON.parse(this.responseText);
+      if (data.access_token != undefined) {
+        access_token = data.access_token;
+        localStorage.setItem("access_token", access_token);
+      }
+      if (data.refresh_token != undefined) {
+        refresh_token = data.refresh_token;
+        localStorage.setItem("refresh_token", refresh_token);
+        localStorage.setItem("token", refresh_token);
+
+      }
+      onPageLoad();
+    }
+    else {
+      console.log(this.responseText);
+    }
+  }
+  function handleRedirect() {
+    let cID = localStorage.getItem("client_id");
+    let cSecret = localStorage.getItem("client_secret");
+    let code = null;
+    const queryString = window.location.search;
+    if (queryString.length > 0) {
+      const urlParams = new URLSearchParams(queryString);
+      code = urlParams.get('code')
+      let body = "grant_type=authorization_code";
+      body += "&code=" + code;
+      body += "&redirect_uri=" + encodeURI(REDIRECT_URI);
+      body += "&client_id=" + cID;
+      body += "&client_secret=" + cSecret;
+      let xhr = new XMLHttpRequest();
+      xhr.open("POST", "https://accounts.spotify.com/api/token", true);
+      xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      xhr.setRequestHeader('Authorization', 'Basic ' + btoa(CLIENT_ID + ":" + clientSecret));
+      xhr.send(body);
+      xhr.onload = handleAuthorizationResponse;
+    }
+  }
+
+  function onPageLoad() {
+    if (window.location.search.length > 0) {
+      handleRedirect();
+
+    }
+    else {
+      access_token = localStorage.getItem("access_token");
+    }
+  }
+  useEffect(() => {
+    setCLIENT_ID(localStorage.getItem("client_id"));
+    setClientSecret(localStorage.getItem("client_secret"))
+    setToken(localStorage.getItem("token"))
+    onPageLoad();
+  }, [])
   return (
     <div>
       <header className='HeaderStyle'>
         <label className='title'>Spotify Artist Search</label>
       </header>
       <div>
-        {!token ?
+        {!token && !loginBox ?
           <div className="CenterBox">
-            <a className="LoginBox" href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>
-              Login <BsSpotify className="SpotifyIcon" /></a>
+            <button className="LoginBox" onClick={() => { setLoginBox(true) }}>
+              Login <BsSpotify className="SpotifyIcon" /></button>
           </div>
           : null}
+        {!token && loginBox ? (
+          <>
+            <div className="CenterBox">
+              <div className="row">
+                <div className="col-6">
+                  <div className="input-group mb-3">
+                    <input type="text" className="form-control" placeholder="Client Id" onChange={(e) => setCLIENT_ID(e.target.value)} aria-label="Client Id" />
+                  </div>
+                </div>
+                <div className="col-6">
+                  <div className="input-group mb-3">
+                    <input type="text" className="form-control" placeholder="Client Secret" onChange={(e) => setClientSecret(e.target.value)} aria-label="Client Secret" />
+                  </div>
+                </div>
+              </div>
+              <div className="row ms-3">
+                <div className="col-12">
+                  <button className="LoginBtn" type="button" onClick={requestAuthorization}>Login In</button>
+                </div>
+              </div>
+            </div>
+          </>
+        ) : null}
         {token && showArtists & albumShow ?
           <>
             <form onSubmit={searchArtists}>
